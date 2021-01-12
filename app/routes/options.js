a751c1b2a21c217c643a8bf68479cdc04cf9822f
thebigpotatoe <thebigpotatoe@gmail.com>
2021-01-12 12:12:30 +0800

    Added injection of new options through the options endpoint

--- app/routes/options.js
+++ app/routes/options.js
@@ -8,27 +8,38 @@ let router = express.Router();
 // Import body parser
 var bodyParser = require('body-parser');
 
+// Import Middleware
+const middleware_options = require('../middleware/middleware_options');
+
 // Import Helpers
 const parse_model_options = require('../helpers/parse_model_options');
 const parse_detection_options = require('../helpers/parse_detection_options');
 
 // Define routes
-router.get('/options', async function (req, res, next) {
-    try {
-        // Send response data
-        res.send({
-            'model_options': req.app.locals.model_options,
-            'detection_options': req.app.locals.detection_options,
-            'no_face_matchers': req.app.locals.face_matchers.length,
-            'weights_route': process.env.PORT,
-            'weights_path': process.env.WEIGHTS_PATH,
-            'descriptors_path': process.env.DESCRIPTOR_PATH
-        });
-    }
-    catch (err) {
-        next(err);
-    }
-})
+router.get('/options',
+    middleware_options,
+    async function (req, res, next) {
+        try {
+            // Save the model options to the app
+            req.app.locals.model_options = req.model_options
+
+            // Save the detection options to the app 
+            req.app.locals.detection_options = req.detection_options
+
+            // Send response data
+            res.send({
+                'model_options': req.app.locals.model_options,
+                'detection_options': req.app.locals.detection_options,
+                'no_face_matchers': req.app.locals.face_matchers.length,
+                'weights_route': process.env.PORT,
+                'weights_path': process.env.WEIGHTS_PATH,
+                'descriptors_path': process.env.DESCRIPTOR_PATH
+            });
+        }
+        catch (err) {
+            next(err);
+        }
+    })
 router.post('/options', bodyParser.json(), async function (req, res, next) {
     try {
         // Parse any valid model options
